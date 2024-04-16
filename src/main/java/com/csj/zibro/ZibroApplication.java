d2b014a149051e85e653a15d86d86bc0b77b4169
ekchoi <dmdrudk@naver.com>
2024-04-16 17:41:39 +0900

    test

---
+++ src/main/java/com/csj/zibro/ZibroApplication.java
@@ -0,0 +1,14 @@
+package com.csj.zibro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZibroApplication {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        SpringApplication.run(ZibroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csj/zibro/ZibroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csj.zibro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZibroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
