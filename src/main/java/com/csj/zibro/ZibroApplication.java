534c5f2cd859d1cc1cf17cacfe66da2dd3cda8a7
ekchoi <dmdrudk@naver.com>
2024-04-22 10:39:56 +0900

    테스트입니다만

--- src/main/java/com/csj/zibro/ZibroApplication.java
+++ src/main/java/com/csj/zibro/ZibroApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZibroApplication {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("테스트중임다");
         SpringApplication.run(ZibroApplication.class, args);
     }
 
